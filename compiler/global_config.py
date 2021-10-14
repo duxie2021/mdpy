929f2e62c43b09c2cb0e52fe9e1f0b1b4d79781c
waqiju <waqiju@gmail.com>
2021-10-14 22:10:06 +0800

    Fix: image path wrong

--- compiler/global_config.py
+++ compiler/global_config.py
@@ -8,5 +8,9 @@ assets_folder_path = ''
 def set_workspace_path(path):
     global workspace_path
     global assets_folder_path
-    workspace_path = path.replace('\\', '/')
-    assets_folder_path = os.path.join(workspace_path, 'assets').replace('\\', '/')
+    # 绝对路径转换
+    workspace_path = os.path.abspath(path)
+    assets_folder_path = os.path.join(workspace_path, 'assets')
+    # 路径分隔符转换
+    workspace_path = workspace_path.replace('\\', '/')
+    assets_folder_path = assets_folder_path.replace('\\', '/')
